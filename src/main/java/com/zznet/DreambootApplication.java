5ecb41bd7ba3e9fea56f1376cef2e08280b79c9b
zzylmt <zzylmt@gmail.com>
2017-07-09 15:41:53 +0800

    20170709

--- src/main/java/com/zznet/DreambootApplication.java
+++ src/main/java/com/zznet/DreambootApplication.java
@@ -1,13 +1,17 @@
 package com.zznet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DreambootApplication {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(DreambootApplication.class, args);
@@ -17,6 +21,9 @@ public class DreambootApplication {
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
         factory.setPort(9010);
+
+        System.out.println(env.getProperty("spring.datasource.username"));
+
         return factory;
     }
 }
