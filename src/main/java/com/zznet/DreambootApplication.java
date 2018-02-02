0368252018c4dd1d96bc9e08eee620694d56dc4c
zzylmt <zzylmt@gmail.com>
2018-02-02 14:48:03 +0800

    20180202

--- src/main/java/com/zznet/DreambootApplication.java
+++ src/main/java/com/zznet/DreambootApplication.java
@@ -1,26 +1,23 @@
 package com.zznet;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class DreambootApplication {
-    @Autowired
-    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(DreambootApplication.class, args);
     }
 
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        factory.setPort(9010);
-        return factory;
-    }
+
+
 }
