769ff7e7ebab19ed0fb9e5360acd303fa3b117fb
zzylmt <zzylmt@gmail.com>
2018-03-02 10:13:46 +0800

    20180302

--- src/main/java/com/zznet/DreambootApplication.java
+++ src/main/java/com/zznet/DreambootApplication.java
@@ -1,15 +1,7 @@
 package com.zznet;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class DreambootApplication {
